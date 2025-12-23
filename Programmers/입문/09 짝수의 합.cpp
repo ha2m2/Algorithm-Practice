dea706275e197706d8937cb2296b01eef54166ec
ha2m2 <hamin0691@gmail.com>
2025-12-23 10:00:21 +0900

    Programmers_짝수의 합

---
+++ Programmers/입문/09 짝수의 합.cpp
@@ -0,0 +1,12 @@
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+int solution(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            sum += i;
+    }
+    return sum;
+}
