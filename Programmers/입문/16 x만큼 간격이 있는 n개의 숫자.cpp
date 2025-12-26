6326d5c2b3d09bcb4c74ad1628360c7e12398e4c
ha2m2 <hamin0691@gmail.com>
2025-12-26 10:01:33 +0900

    Programmers_x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/입문/16 x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,17 @@
+// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n)
+{
+    vector<long long> answer;
+    for (int i = x; n > 0; n--)
+    {
+        answer.push_back(i);
+        i += x;
+    }
+    return answer;
+}
