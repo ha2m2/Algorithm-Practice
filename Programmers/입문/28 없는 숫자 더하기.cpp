d81257a5d2feb3482902472723013de310aebe77
ha2m2 <hamin0691@gmail.com>
2026-01-07 09:54:48 +0900

    Programmers_없는 숫자 더하기

---
+++ Programmers/입문/28 없는 숫자 더하기.cpp
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/86051
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers)
+{
+    int answer = 45;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        answer -= numbers[i];
+    }
+    return answer;
+}
