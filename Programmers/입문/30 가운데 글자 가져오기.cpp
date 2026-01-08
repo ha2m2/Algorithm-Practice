a5ad0d03deceac031bed4ea60f8b1332ff04448b
ha2m2 <hamin0691@gmail.com>
2026-01-08 09:54:53 +0900

    Programmers_가운데 글자 가져오기

---
+++ Programmers/입문/30 가운데 글자 가져오기.cpp
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s)
+{
+    return s.size() % 2 ? s.substr(s.size() / 2, 1) : s.substr(s.size() / 2 - 1, 2);
+}
