f8fcc848270cd55b14b78bba07776cb9a297c242
ha2m2 <hamin0691@gmail.com>
2026-01-08 09:55:03 +0900

    Programmers_수박수박수박수박수박수

---
+++ Programmers/입문/31 수박수박수박수박수박수.cpp
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12922
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n)
+{
+    string answer = "";
+
+    for (int i = 1; i <= n; i++)
+    {
+        answer += i % 2 ? "수" : "박";
+    }
+
+    return answer;
+}
