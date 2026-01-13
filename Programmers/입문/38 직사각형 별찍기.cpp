5274970152380c5e00f12e492c84a1aef53baa84
ha2m2 <hamin0691@gmail.com>
2026-01-13 09:33:35 +0900

    Programmers_직사각형 별찍기

---
+++ Programmers/입문/38 직사각형 별찍기.cpp
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n; // 가로
+    int m; // 세로
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
