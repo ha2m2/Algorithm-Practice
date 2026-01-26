fae017ba3b36087120e1defd187dbb7e2afb20c8
ha2m2 <hamin0691@gmail.com>
2026-01-26 22:57:56 +0900

    Programmers_K번째수

---
+++ Programmers/입문/48 K번째수.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands)
+{
+    vector<int> answer;
+
+    for (const auto &com : commands)
+    {
+        vector<int> temp(array.begin() + com[0] - 1, array.begin() + com[1]);
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[com[2] - 1]);
+    }
+
+    return answer;
+}
